a030a6a4e4b4cf4424a433f31857cf45afbfc1e1
PavelZaika <zpk.loki@gmail.com>
2018-09-09 21:25:58 +0300

    test GoogleSearch

--- src/test/java/com/Google/test.java
+++ src/test/java/com/Google/test.java
@@ -1,6 +1,34 @@
 package com.Google;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class test {
 
-    
+    @Test
+    public void googleSearch() {
+        Configuration.browser = "Chrome";
+        WebDriverManager.chromedriver().setup();
+        open("https://www.google.com/");
+        $(By.name("q")).setValue("Selenium").pressEnter();
+
+
+        $$("#ires div.g").shouldHave(sizeGreaterThan(10));
+        $("#ires div.g").shouldHave(text("selenium — это инструмент для автоматизации"));
+        System.out.println("Done");
+
+    }
+
+
 }
